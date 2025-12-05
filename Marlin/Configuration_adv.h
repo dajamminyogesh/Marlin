862563e1cfa7f7c6975e2ba43342289d8e150bc5
Yogesh Sharma <YogeshASharma@gmail.com>
2025-12-05 17:18:19 -0500

    Enabled Host action commands

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3907,7 +3907,7 @@
  * Host Prompt Support enables Marlin to use the host for user prompts so
  * filament runout and other processes can be managed from the host side.
  */
-//#define HOST_ACTION_COMMANDS
+#define HOST_ACTION_COMMANDS
 #if ENABLED(HOST_ACTION_COMMANDS)
   //#define HOST_PAUSE_M76                // Tell the host to pause in response to M76
   //#define HOST_PROMPT_SUPPORT           // Initiate host prompts to get user feedback
@@ -4290,7 +4290,7 @@
 //#define MARLIN_TEST_BUILD
 
 // Enable Marlin dev mode which adds some special commands
-#define MARLIN_DEV_MODE
+//#define MARLIN_DEV_MODE
 
 #if ENABLED(MARLIN_DEV_MODE)
   /**
